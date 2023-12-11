c25d278736f140380ac16fdee1f71ad72e9552a0
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2023-12-11 17:47:38 +0000

    functionally working task5 displays a single object requires formatting later

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { UserProvider } from "./contexts/UserContext";
 import Users from "./components/Users";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Article from "./components/Article"
 
 // import ReactDom from 'react-dom/client';
 import { Routes, Route } from "react-router-dom";
@@ -25,6 +26,7 @@ function App() {
           <Route path="/users" element={<Users />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/topics" element={<Topics />} />
+          <Route path="/articles/:article_id" element={<Article />} />
         </Routes>
       </UserProvider>
     </>
